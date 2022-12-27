f32b38a46ca35a033f6f317bf37c2c540e227bc0
Bender999 <97208220+Bender999@users.noreply.github.com>
2022-12-27 16:10:46 -0500

    Counter doesnt work

---
+++ magic.js
@@ -0,0 +1,11 @@
+
+
+
+const button = document.getElementById('button');
+
+let count = 0;
+
+button.addEventListener('click', function(){
+    count ++;
+    document.getElementById('counter').innerHTML = count;
+});
