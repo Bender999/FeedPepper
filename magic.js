f44917d641c0cae328e4f519fbfa26295f9350c6
Bender999 <97208220+Bender999@users.noreply.github.com>
2022-12-27 21:59:54 -0500

    adding a countdown timer

--- magic.js
+++ magic.js
@@ -2,14 +2,43 @@
 
 
 const button = document.getElementById('button');
+let countdown = document.getElementById("countdown")
+let message = document.getElementById("message")
 
 let count = 0;
 
+
+
+
 button.addEventListener('click', function(){
     count ++;
-    document.getElementById('counter').innerHTML = count;
     
+    document.getElementById('counter').innerHTML = count;
+    let timer = 10;
+    
+    
+    
+    setInterval(function(){
+        countdown.innerHTML = timer;
+        timer --;
+
+        if (count >= 5 && timer > 0) {
+            
+            message.style.display= "block";
+            message.innerHTML="Pepper is now Full. Thank you!";
+        }else if (count < 5 && timer === 0) {
+            
+            message.style.display = "block";
+            message.innerHTML = "Pepper starved to death!";
+        if (timer < 0){
+            clearInterval(this);
+            }
+            setTimeout(function() {
+                location.reload();
+            });
+        }
+    
+    }, 1000);
 
 
-
-});S
+})
