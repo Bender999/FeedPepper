99924b770476bc9a7a918a73334ee0e3d2e92848
Bender999 <97208220+Bender999@users.noreply.github.com>
2022-12-29 16:15:21 -0500

    border color changes

--- magic.js
+++ magic.js
@@ -27,45 +27,52 @@ const picture = document.getElementById('picture');
 
 let count = 0;
 
+
 button.addEventListener('click', function(){
     picture.style.transform = 'translatey(1000%)';
     picture.style.opacity = '0';
+    
 
 })
 
-button.addEventListener('click', function(){
-    count ++;
-    
 
+
+
+
+
+
+
+button.addEventListener('click', function(){
+    count ++;   
 
     document.getElementById('counter').innerHTML = count; 
                    
         if (count >= 1 && count < 5) {
-            
+            document.body.style.borderColor = "green";
             message.style.display= "block";
             message.innerHTML="Shes a hungry pup";
         }else if (count >= 5 && count <10) {
-            
+            document.body.style.borderColor = "blue";
             message.style.display = "block";
             message.innerHTML = "She loves to eat";
         }else if (count >=10 && count <25){
-            clearInterval(this);
+            document.body.style.borderColor = "orange";
             message.style.display = "block";
             message.innerHTML = "Ok, shes had enough"
         }else if (count >=25 && count <50){
-            clearInterval(this);
+            document.body.style.borderColor = "yellow";
             message.style.display = "block";
             message.innerHTML = "Looks like shes sick"
         }else if (count >=50 && count <75){
-            clearInterval(this);
+            document.body.style.borderColor = "blueviolet";
             message.style.display = "block";
             message.innerHTML = "Shes very sick"
         }else if (count >=75 && count <100){
-            clearInterval(this);
-            message.style.display = "block";
+            document.body.style.borderColor = "pink";
+            message.style.display = "black";
             message.innerHTML = "Shes not moving, stop it!"
         }else if (count >=100 && count <105){
-            clearInterval(this);
+            document.body.style.borderColor = "red";
             message.style.display = "block";
             message.innerHTML = "STOOOOOOOOOP!!"
         }else if (count === 105){
